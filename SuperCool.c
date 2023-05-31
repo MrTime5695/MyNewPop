1109212f6d7f1a5b76d7efd764acd81bc33e6dc4
MrTime5695 <loped132mar@gmail.com>
2023-05-30 20:04:32 -0700

    Bean Counter function added

--- SuperCool.c
+++ SuperCool.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+void CountMyBeans(int beans);
+
 
 int main()  {
 
   puts("dfhnajlnafdsjd");
-
+  CountMyBeans(15);
   return 0;
+}
+
+void CountMyBeans(int beans){
+   int i;
+   for(i = 0; i < beans; i++)
+      printf("I have %d Beans\n", i);
+
 }
